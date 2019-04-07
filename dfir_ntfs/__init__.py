fb440b4e54b2c2f833a4bdbf782dc5f8587238b4
msuhanov <fuf@localhost>
2019-04-08 02:33:24 +0300

    Minor update.

--- dfir_ntfs/__init__.py
+++ dfir_ntfs/__init__.py
@@ -1,5 +1,5 @@
 # dfir_ntfs: an NTFS parser for digital forensics & incident response
 # (c) Maxim Suhanov
 
-__version__ = '1.0.0-beta8'
+__version__ = '1.0.0-beta9'
 __all__ = [ 'MFT', 'Attributes', 'WSL', 'USN', 'LogFile' ]
