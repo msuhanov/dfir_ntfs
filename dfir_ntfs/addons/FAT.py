ec3ae0884048ade106c91a68dab7c9fb3af393bf
Maxim Suhanov <no.spam.c@mail.ru>
2025-10-31 14:39:10 +0300

    Minor update.

--- dfir_ntfs/addons/FAT.py
+++ dfir_ntfs/addons/FAT.py
@@ -77,7 +77,9 @@ FILE_ATTR_LIST = { # ATTR_LONG_NAME is not listed on purpose.
 #   "The following characters are not legal in any bytes of DIR_Name: [...] 0x2E [...]".
 #
 # This is obviously wrong for the dot and dot-dot entries.
-FORBIDDEN_CHARACTERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 42, 43, 44, 46, 47, 58, 59, 60, 61, 62, 63, 91, 92, 93, 124]
+#
+# Also, the '+' character is allowed here (observed on some old Android devices).
+FORBIDDEN_CHARACTERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 42, 44, 46, 47, 58, 59, 60, 61, 62, 63, 91, 92, 93, 124]
 
 # A relaxed version for volume labels.
 FORBIDDEN_CHARACTERS_LABEL = [0, 10, 13]
